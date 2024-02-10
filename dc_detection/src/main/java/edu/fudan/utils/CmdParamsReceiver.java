802e789973d11a214be4050d1f775b269eaa4a96
Lingfeng <664139592@qq.com>
2024-02-10 23:59:09 +0800

    封装命令行工具，测试编译、构建、运行

---
+++ dc_detection/src/main/java/edu/fudan/utils/CmdParamsReceiver.java
@@ -0,0 +1,17 @@
+package edu.fudan.utils;
+
+import lombok.ToString;
+import picocli.CommandLine.Option;
+
+@ToString
+public class CmdParamsReceiver {
+
+  @Option(names = "-ds_id", description = "The dataset id")
+  public int dsId;
+
+  @Option(names = "-dc_path", description = "The DCs path")
+  public String dcPath;
+
+  @Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
+  public boolean helpRequested = false;
+}
